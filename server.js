415c5fadfed36ee6d57cfda71546fa53cd32e9d9
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2026-03-07 11:10:36 +0530

    server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const app = require("./src/app")
 const connect = require("./src/db/db")
-
-
 const PORT = process.env.PORT
-
 app.listen(PORT , ()=>{
     console.log("SERVER IS RUNING  ON PORT NO : ", PORT);
     connect()
